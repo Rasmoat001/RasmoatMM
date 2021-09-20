7573a502857b6a8bc49b4d08dc3634ba70236284
Rasmoat001 <rasmoatyaya1997@gmail.com>
2021-09-20 12:18:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+API_KEY = 'CRank8wS1yXWxI0f2vMvyYxdDXbBACy2k5uTpzbcrKe4SMOHSCf5c64TyujLOYZK'
+API_SECRET = 'sJVVAtrzFZ03npLZ7bbluBp9pM3dS0CdHYGMVvEgooLpWKIScU0YK7kEriNrOQ2Y'
+
+WEBHOOK_PASSWORD = 'Rashidtradingview00'
+
+# Not developed yet :)
+EMAIL = ""
+PASSWORD = ""
